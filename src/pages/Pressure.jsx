c0d86d9629c401be8aa9651d74604a83fd1dffb6
[Joshua Taiwo] <[joshuataiwo07@gmail.com]>
2024-03-28 18:28:31 +0100

    added pressure ui

--- src/pages/Pressure.jsx
+++ src/pages/Pressure.jsx
@@ -2,27 +2,145 @@ import React from 'react'
 import { formatTimestamp } from '../AppManager';
 
 export default function Pressure({ current }) {
-  const [pressure, setPressure] = React.useState(0);
-
-    React.useEffect(() => {
+  const [pressure, setPressure] = React.useState(false);
+  const [value, setValue] = React.useState(0);
+  
+  React.useEffect(() => {
         setTimeout(() => {
           setPressure(current?.Pressure);
-        }, 500);
+       }, 500);
     }, [current]);
+
+  React.useEffect(() => {
+    const interval = 1;
+    const time = setInterval(() => {
+      if(pressure){
+        if (value < parseFloat(pressure)) {
+          setValue((prevValue) => prevValue + 1.4);
+        } else {
+          setValue(parseFloat(pressure));
+          clearInterval(time);
+        }
+      }
+    }, interval);
+
+    return () => {
+      clearInterval(time);
+    };
+  }, [pressure, value]);
 
   return (
     <div className='pressure'>
-        <div role="progressbar" aria-valuenow={pressure.toString()} aria-valuemin="0" aria-valuemax="200" style={{"--value": pressure}}></div>
-        <div className='data-label'>
-          <span>
-            <strong>Pressure:</strong>
-            {pressure} HPa
-          </span>
-          <span>
-            <strong>Time Taken:</strong>
-            {current ? formatTimestamp(current?.Timestamp) : ""}
-          </span>
+      <div className="wrapper">
+        <div className="container">
+          <div className="clock">
+            <p><span>{value.toFixed(1)}hPa</span></p>
+          </div>
+          <ul className="clockline" id="clockline">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div className='time-stamp'>Time Taken: {current && formatTimestamp(current?.Timestamp)}</div>
         </div>
+      </div>
     </div>
   )
 }
